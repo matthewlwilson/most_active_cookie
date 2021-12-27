c48601c99ebe79cfa287c45db6a0ad41a60a4810
Matt Wilson <matthewlwilson135@gmail.com>
2021-12-27 15:26:15 -0800

    read_cookie_log func made/tested

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     arg = functions.get_args()
     filename = functions.get_cookie_filename(arg)
     DateToSearch = functions.get_search_date(arg)
-    functions.read_cookie_log(filename)
+    arryOfCookies = functions.read_cookie_log(filename)
 
 
 if __name__ == '__main__':
